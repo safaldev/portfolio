f27dd237f8a4699c548ec51295ec42d20cf88ecf
safaldev <safalchhetri205@gmail.com>
2025-12-23 12:11:47 +0000

    Bug Fixes

--- src/pages/Dev.jsx
+++ src/pages/Dev.jsx
@@ -5,9 +5,9 @@ function Dev() {
     <>
         <main>
 
-            <h2 className='text-7xl font-extrabold text-orange-600 text-center py-[28dvh]'>This Page Is Currently <br /> Under Maintainance
-            <h3 className='text-xl text-primary font-semibold pt-12'>Mail to: contact@safalchhetri.com.np</h3>
-            <h3 className='text-xl text-primary font-semibold pt-4'>Call Me: +977 9826608987</h3>
+            <h2 className='text-3xl lg:text-7xl font-extrabold text-orange-600 text-center py-[28dvh]'>This Page Is Currently <br /> Under Maintainance
+            <h3 className='text-lg lg:text-xl text-primary font-semibold pt-12'>Mail to: contact@safalchhetri.com.np</h3>
+            <h3 className='text-lg lg:text-xl text-primary font-semibold pt-4'>Call Me: +977 9826608987</h3>
             </h2>
             
         </main>
